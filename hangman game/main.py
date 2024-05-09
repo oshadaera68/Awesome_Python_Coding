d9c9abe1b9c965d57a9c34c1279d4e5b1e4ffd10
oshadaera68 <mindartlk010@gmail.com>
2024-05-09 08:50:51 +0530

    hangman game

---
+++ hangman game/main.py
@@ -0,0 +1,35 @@
+import random
+
+with open('wordlist.txt', 'r') as f:
+    words = f.readlines()
+
+word = random.choice(words)[:-1]
+
+allowed_errors = 7
+guesses = []
+done = False
+
+while not done:
+    for letter in word:
+        if letter.lower() in guesses:
+            print(letter, end=" ")
+        else:
+            print("_", end=" ")
+    print("")
+
+    guess = input(f"Allowed Errors left {allowed_errors}, Next Guess: ")
+    guesses.append(guess.lower())
+    if guess.lower() not in word.lower():
+        allowed_errors -= 1
+        if allowed_errors == 0:
+            break
+
+    done = True
+    for letter in word:
+        if letter.lower() not in guesses:
+            done = False
+
+if done:
+    print(f'You found the word! It was {word}!')
+else:
+    print(f'Game Over! The word was {word}')

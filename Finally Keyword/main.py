e1c65b3470208ea3db5104bfc4956ed86800f8f7
oshadaera68 <mindartlk010@gmail.com>
2024-05-24 10:46:16 +0530

    finally keyword

---
+++ Finally Keyword/main.py
@@ -0,0 +1,28 @@
+# try:
+#     print(10 / int(input("Enter Number: ")))
+#     exit()
+# except ZeroDivisionError:
+#     print("Can not divide by zero")
+#     int('hellp')
+# finally:
+#     print("Final action")
+
+# def myfunction():
+#     try:
+#         return 'try'
+#     except:
+#         return 'except'
+#     finally:
+#         return 'finally'
+#
+#
+# print(myfunction())
+
+while True:
+    try:
+       print('Hello World')
+       break
+    except:
+        print("Error")
+    finally:
+        print("Can not divide by zero")
